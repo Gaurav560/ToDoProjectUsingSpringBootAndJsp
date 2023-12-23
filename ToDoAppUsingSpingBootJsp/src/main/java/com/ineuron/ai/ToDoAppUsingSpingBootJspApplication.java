4d2e45c514f4555733609ffd07f02f84f89c4f98
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-24 00:58:41 +0530

    setting up structure and initial commit for spring boot jsp

---
+++ ToDoAppUsingSpingBootJsp/src/main/java/com/ineuron/ai/ToDoAppUsingSpingBootJspApplication.java
@@ -0,0 +1,13 @@
+package com.ineuron.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppUsingSpingBootJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppUsingSpingBootJspApplication.class, args);
+	}
+
+}
